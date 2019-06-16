ed22792c27a86f5eaa476ae86a27eb8299735d85
veljkoradenkovic <veljko.radenkovic.128.15@ict.edu.rs>
2019-06-16 15:40:38 +0200

    changes

--- Application/Responses/PageResponse.cs
+++ Application/Responses/PageResponse.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Application.Responses
 {
-    public class PageResponse<T> : List<T>
+    public class PageResponse<T> 
     {
         public int TotalCount { get; set; }
         public int PagesCount { get; set; }
